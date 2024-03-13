f0fc5d38983c0cddb4768dd69db7e7fd8b9f0fb1
jcasimiro <j.casimiro@qstrike.com>
2024-03-13 14:46:35 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <button>
+          Add Logo
+        </button>
+      </nav>
+    </>
+  )
+}
+
+export default App
